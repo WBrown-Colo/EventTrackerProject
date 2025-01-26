525ecc62d2192ab5d59a22c7000846850f1cb890
William Brown <wsbrown.colo@gmail.com>
2025-01-26 14:33:02 -0700

    shapeRepo complete

---
+++ Cigars/src/main/java/com/skilldistillery/cigars/repositories/ShapeRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.cigars.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.cigars.entities.Shape;
+
+public interface ShapeRepository extends JpaRepository<Shape, Integer> {
+
+}
