eaf16908c9bfdfb72e5b404e013bdb96e03e0a6a
William Brown <wsbrown.colo@gmail.com>
2025-01-26 18:22:40 -0700

    originService complete

---
+++ Cigars/src/main/java/com/skilldistillery/cigars/services/OriginService.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.cigars.services;
+
+import java.util.List;
+
+import com.skilldistillery.cigars.entities.Origin;
+
+public interface OriginService {
+
+	List<Origin> findAll();
+	Origin findByOriginId();
+	
+}
