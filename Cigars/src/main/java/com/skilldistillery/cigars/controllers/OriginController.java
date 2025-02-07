777c19d1f749c2466ce25433bc3ee4f49e9b4a21
William Brown <wsbrown.colo@gmail.com>
2025-02-07 13:02:38 -0700

    Configure angular workspace

--- Cigars/src/main/java/com/skilldistillery/cigars/controllers/OriginController.java
+++ Cigars/src/main/java/com/skilldistillery/cigars/controllers/OriginController.java
@@ -3,6 +3,7 @@ package com.skilldistillery.cigars.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skilldistillery.cigars.entities.Origin;
 import com.skilldistillery.cigars.services.OriginService;
 
+@CrossOrigin({"*", "http://localhost/"})
 @RestController
 @RequestMapping("api")
 public class OriginController {
--- Cigars/src/main/java/com/skilldistillery/cigars/controllers/ShapeController.java
+++ Cigars/src/main/java/com/skilldistillery/cigars/controllers/ShapeController.java
@@ -3,6 +3,7 @@ package com.skilldistillery.cigars.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skilldistillery.cigars.entities.Shape;
 import com.skilldistillery.cigars.services.ShapeService;
 
+@CrossOrigin({"*", "http://localhost/"})
 @RestController
 @RequestMapping("api")
 public class ShapeController {
